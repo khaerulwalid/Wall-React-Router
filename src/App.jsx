84573043a9f994eadf606ff0c91af6e0a2c2d644
M Khaerul Walid Qutbi <ckrwislam@gmail.com>
2023-11-15 23:59:04 +0800

    Menambah Header, Footer, Home dengan Library React Router Dom

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className=" bg-green-600">React Router Dom</div>;
+}
+
+export default App;
